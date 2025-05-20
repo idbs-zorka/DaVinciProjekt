ff88c417d3482fd6280de134ed31db0d9b8eaaeb
masnyjimmy <kspark470@gmail.com>
2025-05-20 14:22:28 +0200

    Map server

--- src/gui/station_select.py
+++ src/gui/station_select.py
@@ -191,10 +191,9 @@ class StationSelectFrame(ttk.Frame):
         self.index_cb.current(0)
 
         self.map = tkintermapview.TkinterMapView(
-            right,
-            database_path="offline_map_data.db"
+            right
         )
-        self.map.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga")
+        self.map.set_tile_server("http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg")
         self.map.pack(side='bottom', fill='both',expand=True)
         self.map.max_zoom = 10
         self.map.min_zoom = 10
